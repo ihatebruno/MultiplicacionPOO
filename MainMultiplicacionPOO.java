95b707e23bf750584ac4cd8e247d5af9abb841b7
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-05-09 16:51:38 -0600

    Mulitiplicacion POO comentado

--- MainMultiplicacionPOO.java
+++ MainMultiplicacionPOO.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
+
 public class MainMultiplicacionPOO {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        MultiplicacionPOO mult = new MultiplicacionPOO();
-        
+        Scanner sc = new Scanner(System.in); // Crear un objeto Scanner para leer la entrada del usuario.
+        MultiplicacionPOO mult = new MultiplicacionPOO(); // Crear un objeto MultiplicacionPOO.
+
+        // Solicitar al usuario que ingrese los dos números.
         System.out.println("Ingresa el primer numero: ");
-        mult.setA(sc.nextInt());
+        mult.setA(sc.nextInt()); // Leer el primer número y asignarlo a 'a' mediante el método setA.
 
         System.out.println("Ingresa el segundo numero: ");
-        mult.setB(sc.nextInt());
+        mult.setB(sc.nextInt()); // Leer el segundo número y asignarlo a 'b' mediante el método setB.
 
-        System.out.println(mult.multiplicar());
-        System.out.println(mult.toString());
+        System.out.println(mult.multiplicar()); // Imprimir el resultado de la multiplicación.
+        System.out.println(mult.toString()); // Imprimir la representación en cadena del objeto MultiplicacionPOO.
 
-        mult.getA();
-        mult.getB();
+        mult.getA(); // Este método se llama, pero su valor de retorno no se usa ni se imprime.
+        mult.getB(); // Este método se llama, pero su valor de retorno no se usa ni se imprime.
     }
 }
--- MultiplicacionPOO.java
+++ MultiplicacionPOO.java
@@ -1,27 +1,38 @@
 public class MultiplicacionPOO {
-    private int a,b, resultado;
+    private int a, b, resultado; // Definición de variables de instancia para los números a multiplicar y el resultado.
     public int getA() {
         return a;
     }
-    public int multiplicar(){
-        resultado = this.a * this.b;
-        return resultado;
+
+    // Método para realizar la multiplicación.
+    public int multiplicar() {
+        resultado = this.a * this.b; // Realiza la multiplicación y guarda el resultado en la variable de instancia resultado.
+        return resultado; // Devuelve el resultado de la multiplicación.
     }
+
     public void setA(int a) {
         this.a = a;
     }
+
     public int getB() {
         return b;
     }
+
     public void setB(int b) {
         this.b = b;
     }
+
     public int getResultado() {
         return resultado;
     }
+
     public void setResultado(int resultado) {
         this.resultado = resultado;
     }
+
+    // Metodos getters y setters
+
+    // Método toString para representar el objeto MultiplicacionPOO como una cadena de texto.
     @Override
     public String toString() {
         return "MultiplicacionPOO [Primer numero=" + a + ", Segundo numero=" + b + ", Resultado=" + resultado + "]";
